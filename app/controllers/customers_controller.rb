3d8ef2217eacd668e2a4a74a5ac88aec28b06fab
mohira <m.ohira@nowall.co.jp>
2016-09-10 17:30:00 +0900

    Customersコントローラにbefore_action追加

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,10 +1,10 @@
 class CustomersController < ApplicationController
+  before_action :set_customer, only: [:show, :edit, :update, :destroy]
   def index
     @customers = Customer.page(params[:page])
   end
 
   def show
-    @customer = Customer.find(params[:id])
   end
 
   def new
@@ -21,11 +21,9 @@ class CustomersController < ApplicationController
   end
 
   def edit
-    @customer = Customer.find(params[:id])
   end
 
   def update
-    @customer = Customer.find(params[:id])
     if @customer.update(customer_params)
       redirect_to @customer
     else
@@ -34,10 +32,8 @@ class CustomersController < ApplicationController
   end
 
   def destroy
-    render :new
-    @customer = Customer.find(params[:id])
     @customer.destroy
-    # redirect_to customers_url
+    redirect_to customers_url
   end
 
   private
@@ -48,5 +44,9 @@ class CustomersController < ApplicationController
       :given_name,
       :email
       )
+  end
+
+  def set_customer
+    @customer = Customer.find(params[:id])
   end
 end
