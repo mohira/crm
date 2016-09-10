aa9dec871ccafe60627d6746de2fb523f5306853
mohira <m.ohira@nowall.co.jp>
2016-09-10 18:58:27 +0900

    コメント削除機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,7 +9,9 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to customer_url(@comment.customer_id)
   end
 
   private
