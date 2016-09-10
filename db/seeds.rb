8f42f7f1040df7bcc842f1b132c0d3af7c3ce293
mohira <m.ohira@nowall.co.jp>
2016-09-11 00:56:08 +0900

    ダミーデータをいろいろ修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,45 +1,182 @@
-Company.create(
-  name: "Google",
-  url: "http://www.google.co.jp/",
-  address: "東京都港区赤坂2-14-27",
-  image_url: "http://images.dailytech.com/nimage/G_is_For_Google_New_Logo_Thumb.png"
-  )
-Company.create(
-  name: "Apple",
-  url: "http://www.apple.co.jp/",
-  address: "東京都新宿区西新宿6-15-1",
-  image_url: "http://kaorich.com/wp/wp-content/uploads/2014/02/slooProImg_20140202085402.jpg"
-  )
-Company.create(
-  name: "Amazon",
-  url: "http://www.amazon.co.jp/",
-  address: "東京都港区赤坂9-7-1",
-  image_url: "http://cdn-img.easyicon.net/png/11342/1134224.gif"
-  )
+customer_family_names = [
+"入山","大家","大和田","小笠原","小嶋","小嶋","佐々木","島崎","白間","田北","谷口","中西","中村","樋渡","宮崎","宮脇","横山","相笠","阿部","市川","兒玉","篠崎","島田","下口","鈴木","田野","中田","藤田","峯岸","向井地","武藤","茂木","湯本","梅田","大島","柏木","加藤","木﨑","後藤","竹内","達家","田名部","福岡","馬","矢吹","横島","渡辺","飯野","伊豆田","岩立","大川","大森","岡田","岡田","川本","北川","北澤","小嶋","込山","佐藤","渋谷","高橋","朝長","西野","野澤","村山","坂口","横山","谷川","佐藤","早坂","佐藤","舞木","岡部","本田","清水","髙橋","吉川","小栗","小田","佐藤","左伴","歌田","横道","服部","野","北","長","近藤","永野","太田","山田","山本","大西","濵","中野","阿部","人見","谷","下尾","濵松","行天","高岡","廣瀬","吉田","福地","寺田","倉野尾","吉野","谷口","下青木","宮里","野村","久保","高橋","西川","山邊","千葉",
+]
 
-Post.create(position_name: "社長")
-Post.create(position_name: "部長")
-Post.create(position_name: "課長")
-Post.create(position_name: "平社員")
+customer_given_names = [
+"杏奈","志津香","南那","茉由","菜月","陽菜","優佳里","遥香","美瑠","香世子","めぐ","智代梨","麻里子","結依","美穂","咲良","由依","萌","マリア","愛美","遥","彩奈","晴香","ひなな","まりや","優花","ちさと","奈那","みなみ","美音","十夢","忍","亜美","綾乃","涼花","由紀","玲奈","ゆりあ","萌咲","美宥","真姫宝","生来","聖菜","嘉伶","奈子","亜衿","麻友","雅","莉奈","沙穂","莉央","美優","彩花","奈々","紗矢","綾巴","早紀","真子","榛香","妃星","凪咲","朱里","美桜","未姫","玲奈","彩希","渚沙","結衣","聖","七海","つむぎ","朱","香純","麟","仁美","麻璃亜","彩音","七瀬","有以","えりな","栞","彩佳","初夏","侑里","有菜","陽菜","玲名","久玲奈","萌恵里","芹佳","奈緒","菜々美","瑠香","桃香","咲友菜","郁海","芽唯","古都音","優里","みう","里緒菜","優莉奈","薫","なつき","華恋","礼奈","美咲","成美","未優","もか","香鈴","莉羅","奈央","怜音","希良","怜","歩夢","千葉",
+]
+customer_images = [
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_A_png%2Firiyama_anna.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_A_png%2Foya_shizuka.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_B_png%2Fowada_nana.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_B_png%2Fogasawara_mayu.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_A_png%2Fkojima_natsuki.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_A_png%2Fkojima_haruna.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_4_png%2Fsasaki_yukari.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_A_png%2Fshimazaki_haruka.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_A_png%2Fshiroma_miru.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_A_png%2Ftakita_kayoko.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_A_png%2Ftaniguchi_megu.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_A_png%2Fnakanishi_chiyori.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_A_png%2Fnakamura_mariko.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_draft_png%2Fhiwatashi_yui.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_K_png%2Fmiyazaki_miho.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_A_png%2Fmiyawaki_sakura.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_K_png%2Fyokoyama_yui.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_K_png%2Faigasa_moe.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_K_png%2Fabe_maria.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_A_png%2Fichikawa_manami.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_K_png%2Fkodama_haruka.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_4_png%2Fshinozaki_ayana.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_K_png%2Fshimada_haruka.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_K_png%2Fshimoguchi_hinana.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_K_png%2Fsuzuki_mariya.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_K_png%2Ftano_yuuka.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_A_png%2Fnakata_chisato.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_A_png%2Ffujita_nana.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_4_png%2Fminegishi_minami.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_4_png%2Fmukaichi_mion.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_A_png%2Fmutou_tomu.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_4_png%2Fmogi_shinobu.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_K_png%2Fyumoto_ami.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_B_png%2Fumeta_ayano.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_B_png%2Foshima_ryoka.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_B_png%2Fkashiwagi_yuki.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_4_png%2Fkato_rena.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_4_png%2Fkizaki_yuria.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_K_png%2Fgoto_moe.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_B_png%2Ftakeuchi_miyu.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_A_png%2Ftatsuya_makiho.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_B_png%2Ftanabe_miku.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_B_png%2Ffukuoka_seina.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_B_png%2Fma_chalin.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_B_png%2Fyabuki_nako.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_B_png%2Fyokoshima_aeri.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_B_png%2Fwatanabe_mayu.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_A_png%2Fiino_miyabi.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_B_png%2Fizuta_rina.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_4_png%2Fiwatate_saho.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_4_png%2Fokawa_rio.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_4_png%2Fomori_miyuu.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_4_png%2Fokada_ayaka.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_4_png%2Fokada_nana.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_B_png%2Fkawamoto_saya.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_4_png%2Fkitagawa_ryoha.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_4_png%2Fkitazawa_saki.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_K_png%2Fkojima_mako.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_4_png%2Fkomiyama_haruka.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_4_png%2Fsato_kiara.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_4_png%2Fshibuya_nagisa.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_B_png%2Ftakahashi_juri.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_B_png%2Ftomonaga_mio.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_4_png%2Fnishino_miki.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_B_png%2Fnozawa_rena.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_4_png%2Fmurayama_yuiri.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fsakaguchi_nagisa.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fyokoyama_yui.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Ftanikawa_hijiri.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fsato_nanami.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fhayasaka_tsumugi.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fsato_akari.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fmougi_kasumi.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fokabe_rin.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fhonda_hitomi.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fshimizu_maria.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Ftakahashi_ayane.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fyoshikawa_nanase.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Foguri_yui.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Foda_erina.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fsato_shiori.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fhidaritomo_ayaka.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160625%2Futada_hatsuka2.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fyokomichi_yuri.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fhattori_yuna.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160625%2Fnoda_hinano2.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fhashimoto_haruna.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fkita_reina.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fchou_kurena2.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fkondo_moeri.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fnagano_serika.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Foota_nao.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fyamada_nanami.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fyamamoto_ruka.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fonishi_momoka.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fhama_sayuna.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fnakano_ikumi.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fabe_mei.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fhitomi_kotone.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Ftani_yuuri.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fshitao_miu.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fhamamatsu_riona.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fgyoten_yurina.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Ftakaoka_kaoru.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fhirose_natsuki.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fyoshida_karen.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Ffukuchi_rena.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160625%2Fterada_misaki2.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fkuranoo_narumi.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fyoshino_miyu.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fyaguchi_moka.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fshimoaoki_karin.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_8_png%2Fmiyazato_rira.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_draft_png%2Fnomura_nao.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_draft_png%2Fkubo_satone.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_draft_png%2Ftakahashi_kira.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_draft_png%2Fnishikawa_rei.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_draft_png%2Fyamabe_ayu.png",
+  "http://cdn.akb48.co.jp/cache/image/?path=%2Fmembers%2Fprofile20160509%2Fteam_draft_png%2Fchiba_erii.png",
+]
 
-User.create(family_name: "八幡平", given_name: "太郎", email: "hachiman@taro.com", password: "hachimantaro", image_url: "http://www.hachimantaishi.com/shokokai/taira/gallery/gallery/63.jpg")
-User.create(family_name: "安比", given_name: "花子", email: "appi@hanako.com", password: "appihanako", image_url: "http://www.appi.co.jp/wp/wp-content/uploads/2014/04/gr_top_03.jpg")
-User.create(family_name: "厚切り", given_name: "ダンソン", email: "atsugiri@danson.com", password: "atsugiridanson", image_url: "http://www.appi.co.jp/wp/wp-content/uploads/2014/04/icecream.jpg")
+companies_num = 100
+posts_num     = 100
+customers_num = 300
 
-300.times do |comment|
-  Comment.create(body: "#{comment}回目のコメントです。",
-                 customer_id: rand(100) + 1,
-                 user_id: rand(3) + 1)
-end
-
+User.create(
+  family_name: "八幡平",
+  given_name: "太郎",
+  email: "hachiman@taro.com",
+  password: "hachimantaro",
+  image_url: "http://www.hachimantaishi.com/shokokai/taira/gallery/gallery/63.jpg"
+)
 
 100.times do |index|
+  key = rand(0..customer_images.size) - 1
   Customer.create(
+    family_name: customer_family_names[key],
+    given_name: customer_given_names[key],
+    email: "customer_#{index}@sparta.com",
+    company_id: rand(1..companies_num),
+    post_id: rand(1..posts_num),
+    image_url: customer_images[key]
+  )
+
+  Company.create(
+    name: Faker::Company.name,
+    url: Faker::Internet.url,
+    address: Faker::Address.street_name,
+    image_url: Faker::Avatar.image,
+  )
+
+  Post.create(
+    position_name: Faker::Pokemon.name
+  )
+
+  User.create(
     family_name: Faker::Japanese::Name.last_name,
     given_name: Faker::Japanese::Name.first_name,
-    email: "customer_#{index}@sparta.com",
-    company_id: rand(3) + 1,
-    post_id: rand(4) + 1,
-    image_url: "http://www.hachimantaishi.com/shokokai/taira/gallery/gallery/63.jpg"
+    email: "sample#{index}@example.com",
+    password: "password#{index}",
+    image_url: Faker::Avatar.image
   )
 end
+
+300.times do |comment|
+  Comment.create(
+    body:  Faker::Lorem.sentence,
+    customer_id: rand(1..customer_images.size),
+    user_id: rand(1..100),
+    created_at: Faker::Time.between(100.days.ago, Date.today, :all),
+  )
+end
+
+
